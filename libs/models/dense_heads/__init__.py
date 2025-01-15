631a71f9162de18404a57901341696c3e661a562
Seasoning <bw_chen1223@foxmail.com>
2025-01-15 17:04:47 +0800

    add querylanev3

--- libs/models/dense_heads/__init__.py
+++ libs/models/dense_heads/__init__.py
@@ -4,3 +4,4 @@ from .spglane_head import SPGLaneHead  # noqa: F401
 from .srcnnlane_head import SRCNNLaneHead
 from .querylane_head import QueryLaneHead
 from .querylane_headv2 import QueryLaneHeadV2
+from .querylane_headv3 import QueryLaneHeadV3
--- libs/models/layers/__init__.py
+++ libs/models/layers/__init__.py
@@ -4,3 +4,4 @@ from .multi_scale_proposal_fusion import MsPFusion
 from .fusion_blocks import CSAM2d
 from .query_update import QueryUpdate
 from .query_roi import QueryROI
+from .query_feat import QueryFeat
